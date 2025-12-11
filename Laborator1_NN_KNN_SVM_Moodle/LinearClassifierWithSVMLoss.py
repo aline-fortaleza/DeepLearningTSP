e434509d26fd089f7d1655286b235f8bb7d59e0b
Aline Fortaleza <affs2@cin.ufpe.br>
2025-12-11 10:44:13 +0100

    SVM without the prediction yet

--- Laborator1_NN_KNN_SVM_Moodle/LinearClassifierWithSVMLoss.py
+++ Laborator1_NN_KNN_SVM_Moodle/LinearClassifierWithSVMLoss.py
@@ -10,13 +10,13 @@ def predict(xsample, W):
     return s
 
 # TODO - Application 3 - Step 3 - The function that compute the loss for a data point
-def computeLossForASample(s, labelForSample, delta):
+def computeLossForASample(s, labelForSample, delta): # s -> score vector output from predict function, label -> true class label, delta -> margin
 
     loss_i = 0
-    syi = s[labelForSample]  # the score for the correct class corresponding to the current input sample based on the label yi
+    syi = s[labelForSample]  # the score for the correct class corresponding to the current input sample based on the label yi -> extracts the score for the true class that we are dealing now
 
     # TODO - Application 3 - Step 3 - compute the loss_i
-    for j, sj in enumerate(s):
+    for j, sj in enumerate(s): # iterate through all the scores for all classes
         dist = sj - syi + delta
 
         if j == labelForSample: # skip the true class
@@ -29,27 +29,24 @@ def computeLossForASample(s, labelForSample, delta):
     return loss_i
 
 # TODO - Application 3 - Step 4 - The function that compute the gradient loss for a data point
+# increases scores for incorrect classes and decreases score for correct class
 def computeLossGradientForASample(W, s, currentDataPoint, labelForSample, delta):
 
-    dW_i = np.zeros(W.shape)  # initialize the matrix of gradients with zero
-    syi = s[labelForSample]   # establish the score obtained for the true class
+    dW_i = np.zeros(W.shape)  # initialize the matrix of gradients with zero -> this will accumulate the gradients for the current sample
+    syi = s[labelForSample]   # establish the score obtained for the true class 
 
-    for j, sj in enumerate(s):
+    for j, sj in enumerate(s): # j is class index, sj is score for class j
         dist = sj - syi + delta
 
-        if j == labelForSample:
+        if j == labelForSample: # doesn't check against itself
             continue
 
         if dist > 0:
-            dW_i[j] = currentDataPoint
-            dW_i[labelForSample] = dW_i[labelForSample] - currentDataPoint
+            dW_i[j] = currentDataPoint # increase the weights for incorrect classes
+            dW_i[labelForSample] = dW_i[labelForSample] - currentDataPoint # gets the gradient of the true class reduced, wich means the program saying " the true class score was too low, adjust the weights now to push it up"
 
     return dW_i
-#####################################################################################################################
-#####################################################################################################################
 
-
-#####################################################################################################################
 #####################################################################################################################
 def main():
 
@@ -90,53 +87,53 @@ def main():
     for idx, xsample in enumerate(x_train):
 
         # TODO - Application 3 - Step 2 - ...compute the scores s for all classes (call the method predict)
-        # s = ...
+        s = predict(xsample, W) # scores for all classes for the current input data point 
 
 
 
         # TODO - Application 3 - Step 3 - Call the function (computeLossForASample) that
         #  compute the loss for a data point (loss_i)
-        #loss_i = ...
+        loss_i = computeLossForASample(s, y_train[idx], delta)
 
 
 
         # Print the scores - Uncomment this
-        # print("Scores for sample {} with label {} is: {} and loss is {}".format(idx, y_train[idx], s, loss_i))
+        print("Scores for sample {} with label {} is: {} and loss is {}".format(idx, y_train[idx], s, loss_i))
 
 
 
         # TODO - Application 3 - Step 4 - Call the function (computeLossGradientForASample) that
         #  compute the gradient loss for a data point (dW_i)
-        #dW_i = computeLossGradientForASample(W, s, x_train[idx], y_train[idx], delta)
+        dW_i = computeLossGradientForASample(W, s, x_train[idx], y_train[idx], delta)
 
 
 
         # TODO - Application 3 - Step 5 - Compute the global loss for all the samples (loss_L)
-        #loss_L = ...
+        loss_L = loss_L + loss_i # accumulate the loss for all samples
 
 
 
         # TODO - Application 3 - Step 6 - Compute the global gradient loss matrix (dW)
-        #dW = ...
+        dW = dW + dW_i # accumulate the gradient for all samples
 
 
 
-        pass    #REMOVE THIS
+
 
 
     # TODO - Application 3 - Step 7 - Compute the global normalized loss
-    #loss_L = ...
-    #print("The global normalized loss = {}".format(loss_L))
+    loss_L = loss_L / len(x_train)  # normalize the loss by the number of training samples
+    print("The global normalized loss = {}".format(loss_L))
 
 
 
     # TODO - Application 3 - Step 8 - Compute the global normalized gradient loss matrix
-    #dW = ...
+    dW = dW / len(x_train) # normalize the gradient by the number of training samples
 
 
 
     # TODO - Application 3 - Step 9 - Adjust the weights matrix
-    #W = ...
+    W = W - step_size * dW  # update the weights by moving in the direction of negative gradient
 
 
 
